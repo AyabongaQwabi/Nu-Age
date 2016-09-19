baea0405bad2deb7c16e604f7b703cd2685b3b78
Ayabonga <aya@westerncapelabs.com>
2016-09-19 22:34:18 +0200

    Created Numero tester

---
+++ test/numero_test.js
@@ -0,0 +1,7 @@
+var numero = require('../numero');
+var sequencer = new numero();
+ var mypassword = 'my email is ayabonga@projectcodex.co please help with #punani I live in 1541'
+//var mypassword = 'i am aya'
+var seqd = sequencer.convert(mypassword)
+console.log(seqd)
+console.log(sequencer.revert(seqd))
