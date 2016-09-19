24c63fa5da629e8331616d31120fa007730a2780
Ayabonga <aya@westerncapelabs.com>
2016-09-19 23:38:58 +0200

    Refactored test using conceal instead of numero

--- test/minx_test.js
+++
@@ -1,7 +0,0 @@
-var minx = require('./minx')
-var minxer = new minx();
-console.log(minxer.minxWord('is'))
-var converted =minxer.minxSentence('freewill is power from the sky')
-console.log('('+converted+')')
-var averted = minxer.minxSentence(converted)
-console.log(averted)
