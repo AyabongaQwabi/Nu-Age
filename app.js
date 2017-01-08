edd5a90a4848501d04fc568ac74a045ab36d6479
Ayabonga <aya@westerncapelabs.com>
2017-01-08 13:07:07 +0200

    Renamed encryption and decryption routes

--- app.js
+++ app.js
@@ -1,53 +1,49 @@
-var express =require('express');
+var express = require('express');
 var handlebars = require('express-handlebars');
 var numero = require('./numero');
 var conceal = require('./conceal');
 var minx = require('./minx'),
-	bodyParser = require('body-parser');
+    bodyParser = require('body-parser');
 
 
 var app = express();
 app.use(express.static('public'))
-app.engine('handlebars',handlebars({defaultLayout:'main'}))
-app.set('view engine','handlebars')
-app.use(bodyParser.urlencoded({ extended: false }));
+app.engine('handlebars', handlebars({
+    defaultLayout: 'main'
+}))
+app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
-app.get('/', function(req,res){
-	res.render('index')
-    //var text = "I am a self-motivated young web apps developer who is passionate about using design and code skills to help solve social, entrepreneurial and enterprise problems."
-    //var sequencer = new numero();
-    //console.log(sequencer.convert(text))
+app.get('/', function(req, res) {
+    res.render('index')
 })
-app.post('/hiroshima',function(req,res){
-	var text = req.body.data;
-  
 
-	var c = new conceal();
-	if(text==''){
-		res.send('...')
-	}
-	else{
-		
+app.post('/conceal', function(req, res) {
+    var text = req.body.data;
+    var c = new conceal();
+    if (text == '') {
+        res.send('...')
+    } else {
         res.send(c.conceal(text))
-	}
+    }
 
 })
-app.post('/nagasaki',function(req,res){
-	var text = req.body.data;
-	
-	var c = new conceal();
-	if(text==''){
-		
-		res.send('...')
-	}
-	else if (text.indexOf('+') == (-1)) {
-		res.send({err : "Santa doesn't give naughty kids presents ..."})
-	}
-	else{
-	
-		res.send(c.unseal(text))
-	}
+app.post('/unseal', function(req, res) {
+    var text = req.body.data;
+    var c = new conceal();
+    if (text == '') {
+        res.send('...')
+    } else if (text.indexOf('+') == (-1)) {
+        res.send({
+            err: "Error String entered is invalid."
+        })
+    } else {
+
+        res.send(c.unseal(text))
+    }
 
 })
 
